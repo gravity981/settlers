7f66b3c7515e2280abadf1fab5752829071966b4
gravity981 <nicolas.marty@gmx.net>
2022-01-05 12:09:05 +0100

    wip implementing trading center

--- src/TradingCenter.cpp
+++ src/TradingCenter.cpp
@@ -1,4 +1,5 @@
 #include <nlohmann/json.hpp>
+#include <spdlog/spdlog.h>
 #include "settlers/TradingCenter.h"
 #include "settlers/WorldGenerator.h"
 
@@ -12,5 +13,34 @@ TradingCenter::~TradingCenter()
 bool TradingCenter::initFromFile(const std::string& filePath)
 {
   nlohmann::json jsonData;
-  return WorldGenerator::readFile(filePath, jsonData);
+  //todo move readFile function to some json helper class
+  if(!WorldGenerator::readFile(filePath, jsonData))
+  {
+    return false;
+  }
+  try
+  {
+    for (auto& obj : jsonData)
+    {
+      auto typeStr = obj["type"].get<std::string>();
+      //todo EBuyableObject from typeString
+      Resource resource;
+      for(auto& costsObj : obj["cost"])
+      {
+        auto resourceStr = costsObj["resource"].get<std::string>();
+        //todo move ResourceFromString to Resource class
+        auto resourceType = Harbour::ResourceFromString(resourceStr);
+        auto amount = costsObj["amount"].get<int>();
+        resource.set(resourceType, amount);
+      }
+      //todo use correct object
+      m_buildingCosts.insert(std::make_pair(BUYABLEOBJECT_ROAD, resource));
+    }
+  }
+  catch (nlohmann::json::type_error& ex)
+  {
+    SPDLOG_ERROR("failed to read building costs: {}", ex.what());
+    return false;
+  }
+  return true;
 }
